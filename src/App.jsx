deb3499d5538f753f9cb73a5e531b9306bed9407
LeMonsalve <devlemonsalve@gmail.com>
2022-12-29 19:02:04 -0500

    Was created the cryptos list and Css file Was aggregated AXIOS

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,40 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
+
+import './styles/App.css'
+
 function App() {
+
+  const API_URL = import.meta.env.VITE_API_URL
+
+  const [cryptos, setCryptos] = useState([]);
+
+  useEffect(() => {
+
+    axios.get(`${API_URL}/assets`)
+      .then( ({ data }) => {
+        setCryptos( data.data )
+      })
+      .catch( err => {
+        console.error("The request is fail: ", err)
+      })
+  }, []);
+
+  if ( cryptos.length < 1 ) return <span>Loading...</span>
+
   return (
     <>
-      <p>Hello world</p>
+      <h2>List of Cryptos</h2>
+      <ol>
+        {
+          cryptos.map(({ id, name, priceUsd }) => (
+            <li key={ id } >Name: { name }, Price: { priceUsd }</li>
+          ))
+        }
+      </ol>
     </>
   )
+
 }
 
 export default App

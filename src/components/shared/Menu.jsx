1a682b52c1669dac94a191c3819bff64fd5b1fac
LeMonsalve <devlemonsalve@gmail.com>
2022-12-30 22:24:16 -0500

    Icon credits

--- src/components/shared/Menu.jsx
+++ src/components/shared/Menu.jsx
@@ -11,6 +11,10 @@ const Menu = () => {
         <li><NavLink to="/" >Home</NavLink></li>
         <li><NavLink to="/crypto" >Crypto list</NavLink></li>
       </ul>
+
+      <a target="_blank"
+         href="https://icons8.com/icon/50914/bitcoin"
+         className="icon-credits" >Bitcoin by icons8</a>
     </nav>
   )
 }
